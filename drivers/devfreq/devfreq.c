d3a80e9526f4d9203a77a36ecb545f3735b5435c
John Galt <johngaltfirstrun@gmail.com>
2023-03-06 15:58:37 -0500

    devfreq: drop WQ_UNBOUND
    Due to asym arm64 latency regression on WQ_UNBOUND.
    
    devfreq workqueue is latency sensitive.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1312,7 +1312,7 @@ static int __init devfreq_init(void)
 	}
 
 	devfreq_wq = alloc_workqueue("devfreq_wq",
-			    WQ_HIGHPRI | WQ_UNBOUND | WQ_FREEZABLE |
+			    WQ_HIGHPRI | WQ_FREEZABLE |
 			    WQ_MEM_RECLAIM, 0);
 	if (!devfreq_wq) {
 		class_destroy(devfreq_class);
